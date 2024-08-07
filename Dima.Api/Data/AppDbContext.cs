2f7b0ca4dfe0488d2980407c4132a61e3897f1ab
tgr <thaiguer.k@gmail.com>
2024-08-06 21:12:13 -0300

    adjusts in appdbcontext

--- Dima.Api/Data/AppDbContext.cs
+++ Dima.Api/Data/AppDbContext.cs
@@ -1,5 +1,15 @@
-﻿namespace Dima.Api.Data;
+﻿using Dima.Core.Models;
+using Microsoft.EntityFrameworkCore;
 
-public class AppDbContext
+namespace Dima.Api.Data;
+
+public class AppDbContext : DbContext
 {
+    public DbSet<Category> Categories { get; set; } = null!;
+    public DbSet<Transaction> Transactions { get; set; } = null!;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration();
+    }
 }
