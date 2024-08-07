a7c805498f6164e1cb0e0621278eb72741b76e48
tgr <thaiguer.k@gmail.com>
2024-08-06 21:44:16 -0300

    create core mappings

---
+++ Dima.Api/Data/Mappings/TransactionMapping.cs
@@ -0,0 +1,38 @@
+﻿using Dima.Core.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Dima.Api.Data.Mappings;
+
+public class TransactionMapping : IEntityTypeConfiguration<Transaction>
+{
+    public void Configure(EntityTypeBuilder<Transaction> builder)
+    {
+        builder.ToTable("Transaction");
+
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Title)
+            .IsRequired()
+            .HasColumnType("NVARCHAR")
+            .HasMaxLength(80);
+
+        builder.Property(x => x.Type)
+            .IsRequired()
+            .HasColumnType("SMALLINT");
+
+        builder.Property(x => x.Ammount)
+            .IsRequired()
+            .HasColumnType("MONEY");
+
+        builder.Property(x => x.CreatedAt)
+            .IsRequired();
+
+        builder.Property(x => x.PaidOrReceivedAt);
+
+        builder.Property(x => x.UserId)
+            .IsRequired()
+            .HasColumnType("NVARCHAR")
+            .HasMaxLength(160);
+    }
+}
