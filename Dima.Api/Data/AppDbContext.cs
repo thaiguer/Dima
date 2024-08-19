7e53f0e6bf074e95644479ac11d9888ecd29b8c6
tgr <thaiguer.k@gmail.com>
2024-08-19 19:54:44 -0300

    db created

--- Dima.Api/Data/AppDbContext.cs
+++ Dima.Api/Data/AppDbContext.cs
@@ -13,6 +13,9 @@ public class AppDbContext (DbContextOptions<AppDbContext> options) : DbContext (
     {
         //gonna search for classes that implement IEntityTypeConfiguration
         modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
-        //Server=localhost,1433;Database=dima-dev;User ID=sa;Password=1q2w3e4r@#$;Trusted_Connection=False; TrustServerCertificate=True;
+        //the connection string is set in dotnet user-secrets
+        //in the format of appsettings.json, that has the default connection empty
+        //dotnet user-secrets set "ConnectionStrings:DefaultConnection" "Server=127.0.0.1,1433;Database=dima-dev;User ID=sa;Password=1q2w3e4r@#$;Trusted_Connection=False; TrustServerCertificate=True;"
+        //Server=127.0.0.1,1433;Database=dima-dev;User ID=sa;Password=1q2w3e4r@#$;Trusted_Connection=False; TrustServerCertificate=True;
     }
 }
