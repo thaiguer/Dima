eec92aca263365d6ad1de988a36e57a3ae32a923
tgr <thaiguer.k@gmail.com>
2024-08-19 19:15:01 -0300

    create ctor for appDbContext

--- Dima.Api/Data/AppDbContext.cs
+++ Dima.Api/Data/AppDbContext.cs
@@ -4,7 +4,7 @@ using System.Reflection;
 
 namespace Dima.Api.Data;
 
-public class AppDbContext : DbContext
+public class AppDbContext (DbContextOptions<AppDbContext> options) : DbContext (options)
 {
     public DbSet<Category> Categories { get; set; } = null!;
     public DbSet<Transaction> Transactions { get; set; } = null!;
