b970bf3202a77ea9c5c3b6aebb5aadd3641d116e
tgr <thaiguer.k@gmail.com>
2024-09-14 11:56:20 -0300

    identity migrations

--- Dima.Api/Data/Mappings/Identity/IdentityUserMapping.cs
+++ Dima.Api/Data/Mappings/Identity/IdentityUserMapping.cs
@@ -15,9 +15,9 @@ public class IdentityUserMapping : IEntityTypeConfiguration<User>
         builder.HasIndex(x => x.NormalizedUserName).IsUnique();
         builder.HasIndex(x => x.NormalizedEmail).IsUnique();
 
-        builder.Property(x => x.Email).HasMaxLength(180);
+        builder.Property(x => x.Email).HasMaxLength(180).IsRequired();
         builder.Property(x => x.NormalizedEmail).HasMaxLength(180);
-        builder.Property(x => x.UserName).HasMaxLength(180);
+        builder.Property(x => x.UserName).HasMaxLength(180).IsRequired();
         builder.Property(x => x.NormalizedUserName).HasMaxLength(180);
         builder.Property(x => x.PhoneNumber).HasMaxLength(20);
         builder.Property(x => x.ConcurrencyStamp).IsConcurrencyToken();
